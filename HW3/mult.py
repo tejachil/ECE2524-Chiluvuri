c69b67b014b5b3f88e22b6bf9a333909c92b394a
Nayana Teja Chiluvuri <tejachil@vt.edu>
2012-09-19 11:20:47 -0400

    Added comments to mult.py

--- HW3/mult.py
+++ HW3/mult.py
@@ -4,22 +4,31 @@
 import sys
 import argparse
 
+# argument parser
 parser = argparse.ArgumentParser(description='Process some integers.')
 args = parser.parse_args()
 
-product = 1.0
+product = 1.0 # initialize the product to 1
 while 1:
-	line = sys.stdin.readline()
+	line = sys.stdin.readline() # read each line of the stdin
+
+	# condition for if <ENTER> is pressed on the stdin without anything being entered
 	if line == '\n':
 		print product
 		product = 1.0
+		
+	# condition for the enf of file
 	elif len(line) == 0:
 		break
+
+	# condition if something is entered on the stdin
 	else:
 		try:
 			product *= float(line)
-		except Exception as err:
+		except Exception as err: # exception if what is entered is not a number
 			sys.stderr.write(str(err))
 			sys.exit(1)
+
+# print the product and exit
 print product
 sys.exit(0)
